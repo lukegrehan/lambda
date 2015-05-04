c90c1afb584984e9f167d0d6704cbddfc9ed6967
Luke Grehan <luketenant@gmail.com>
2015-05-04 23:01:57 +0100

    changed reduce

--- Eval.hs
+++ Eval.hs
@@ -1,4 +1,4 @@
-module Eval (flatten, reduceB, reduceS) where
+module Eval (flatten, reduceB, alpha, beta) where
 import Defs
 import Data.Maybe
 import Data.List
@@ -18,16 +18,21 @@ flatten' (Abs v b) ds = Abs v $ flatten' b ds
 flatten' (App f a) ds = App (flatten' f ds) (flatten' a ds)
 
 reduceB :: Lambda -> Lambda
-reduceB l = go l (reduceS l)
+reduceB l = go l (beta l)
   where 
     go p Nothing = p
-    go _ (Just n) = go n (reduceS n)
+    go _ (Just n) = go n (beta n)
 
-reduceS :: Lambda -> Maybe Lambda
-reduceS (Var _)            = Nothing
-reduceS (Abs f b)          = Nothing
-reduceS (App (Abs v b) ar) = Just $ replace b v ar
-reduceS (App f ar)         = (flip App ar) <$> reduceS f
+alpha :: String -> Lambda -> Lambda
+alpha new (Var s) = Var new
+alpha new (Abs f b) = Abs new $ alpha new b
+alpha new (App f ar) = App f ar
+
+beta :: Lambda -> Maybe Lambda
+beta (Var _)            = Nothing
+beta (Abs f b)          = Nothing
+beta (App (Abs v b) ar) = Just $ replace b v ar
+beta (App f ar)         = (flip App ar) <$> beta f
 
 replace :: Lambda -> String -> Lambda -> Lambda 
 replace (App f a) s ar = App (replace f s ar) (replace a s ar)
@@ -35,3 +40,11 @@ replace (Var s) s' ar   | s == s' = ar
                         | otherwise = Var s
 replace (Abs s b) s' ar | s == s' = Abs s b
                         | otherwise = Abs s (replace b s' ar)
+
+freshVar :: Lambda -> String
+freshVar l = head $ dropWhile (`elem` freeVars l) $ iterate ('_':) 'a'
+
+freeVars :: Lambda -> [String]
+freeVars (Var s) = [s]
+freeVars (Abs f b) = freeVars b \\ [f]
+freeVars (App f ar) = freeVars f `union` freeVars ar
--- t
+++ t
@@ -1,3 +1,7 @@
 I := \x.x
+Zero := \f.\x.x
+One := \f.\x.(f) x
+Two := \f.\x.(f)(f)x
+Const := \x.\y.x
 
-Main := (I) x
+Main := ((Const) y) y
